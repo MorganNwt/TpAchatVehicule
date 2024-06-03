4f0f0b783b1ccc55327f273657b1777aa0f267bf
morganNwt <metodik60@hotmail.fr>
2024-06-03 14:03:11 +0200

    exo3

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
     require_once 'service/db_connect.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -8,6 +7,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/main.css">
+
     <title>TP BDD : cartesgrises</title>
 </head>
 <body>
@@ -16,22 +17,68 @@
 
     <?php
         // Valeurs d'un article à insérer en BDD ( attention si actualisation changer l'id erreur ligne 70 "intégrité" id ligne 59)
-        $id = 1;
-        $immatriculation = 'FQ288KE';
-        $dateCartes = '1999/10/12';
+    //     $id = 1;
+    //     $immatriculation = 'FQ288KE';
+    //     $dateCartes = '1999/10/12';
 
-        // La requête
-        $query = 'INSERT INTO cartesgrises 
-            VALUES (
-                \'' . $id . '\',
-                 \''. $immatriculation . '\' ,
-                  \'' . $dateCartes  .'\' )';
+    //     // La requête
+    //     $query = 'INSERT INTO cartesgrises 
+    //         VALUES (
+    //             \'' . $id . '\',
+    //              \''. $immatriculation . '\' ,
+    //               \'' . $dateCartes  .'\' )';
 
-     $rowCount = $pdo->exec($query);
-     echo '<p>Nombre de lignes insérées = ' . $rowCount . '</p>';
+    //  $rowCount = $pdo->exec($query);
+    //  echo '<p>Nombre de lignes insérées = ' . $rowCount . '</p>';
     ?>
 
     <br><hr><br>
+
+    <h2>Exercice 2</h2>
+    <p> Créer un script permettant d’afficher le contenu de la table « modeles » dans un tableau
+        HTML. Les résultats doivent être triés par marque
+    </p>
+
+    <?php
+        // la requête SELECT
+        $requete = ' SELECT * FROM MODELES ORDER BY marque ASC';
+       
+        // Préparation de la requête
+        $stmt = $pdo->query($requete);
+
+        // Réupération des résultats
+        $listeModeles = $stmt->fetchAll();
+
+      // var_dump($listeModeles);
+    ?>
+
+    <br><hr><br>
+
+    <h3>Exercice 3</h3>
+    <p>Créer un formulaire qui permet l’insertion de nouvelles données dans la table 
+        « modeles ».
+    </p>
+
+    <table>
+        <tr>
+            <th>id_modele</th>
+            <th>Modèle</th>
+            <th>Marque</th>
+            <th>Carburant</th>
+        </tr>
+
+        <?php
+            // Affichage des données
+            foreach($listeModeles as $modele){
+                 echo '<tr class="border">';
+                
+                foreach($modele as $valeur){
+                    echo '<td class="border">' . $valeur . '</td>';
+                }
+                echo '</tr>';
+            }
+        ?>  
+    </table>
 
 
 
--- sql/dml.sql
+++ sql/dml.sql
@@ -6,16 +6,16 @@ DELETE FROM PROPRIETAIRES;
 DELETE FROM CARTESGRISES;
 
 INSERT INTO MODELES
-    VALUES ('1', 'Classe-A', 'Mercedes-Benz', 'Essence'),
-            ('2', 'Classe-C', 'Mercedes-Benz', 'Diesel'),
-            ('3', 'M3', 'BMW', 'Essence'),
-            ('4', 'A3', 'Audi-', 'Electrique');
+    VALUES (DEFAULT, 'Classe-A', 'Mercedes-Benz', 'Essence'),
+            (DEFAULT, 'Classe-C', 'Mercedes-Benz', 'Diesel'),
+            (DEFAULT, 'M3', 'BMW', 'Essence'),
+            (DEFAULT, 'A3', 'Audi', 'Electrique');
 
 INSERT INTO VOITURES
-    VALUES  ('01ABC50', '1', 'MO', '1999-08-18'),
-            ('04EFG49', '2', 'CL', '2005-07-25'),
-            ('03HIJ39', '3', 'FO', '1987-09-28'),
-            ('02KLM24', '4', 'CL', '2020-06-27');
+    VALUES  ('FK288KE', '1', 'MO', '1999-08-18'),
+            ('GU485KF', '2', 'CL', '2005-07-25'),
+            ('TY898PL', '3', 'FO', '1987-09-28'),
+            ('AQ277LM', '4', 'CL', '2020-06-27');
 
 INSERT INTO PROPRIETAIRES
     VALUES  (DEFAULT, 'NAWROT', 'Morgan', '7 allée', 'Valence', '26000'),   
@@ -24,8 +24,8 @@ INSERT INTO PROPRIETAIRES
             (DEFAULT, 'GUY', 'Georges', '20 impasse', 'St eulalie', '26190');
 
 INSERT INTO CARTESGRISES
-    VALUES  ('1', 'FK288KE', '2002-04-18'),
-            ('2', 'GU485KF', '2023-06-17'),
-            ('3', 'TY898PL', '2022-05-15'),
-            ('4', 'AQ277LM', '2018-09-18');
+    VALUES  (DEFAULT, 'FK288KE', '2002-04-18'),
+            (DEFAULT, 'GU485KF', '2023-06-17'),
+            (DEFAULT, 'TY898PL', '2022-05-15'),
+            (DEFAULT, 'AQ277LM', '2018-09-18');
 
